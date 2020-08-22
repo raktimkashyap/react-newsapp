21b6f51ad75d46a822823671f8326bd9945b79ac
raktim@dobbyads.com <raktim@dobbyads.com>
2020-08-23 01:10:37 +0530

    Initial Project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello React News app</h1>
+			<p>This app will be built on NewsAPI and React js</p>
+		</div>
+	);
+}
+
+export default App;
